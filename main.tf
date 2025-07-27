e5f1efa5df14159f6910da29e446f68bf93127e4
akarthikkumarGIT <akarthikkumar@gmail.com>
2025-07-27 16:53:58 -0700

    commit

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+// More information on the Azure Terraform provider is available in the below link
+// https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grps"
+  location = "North Europe"
+}
