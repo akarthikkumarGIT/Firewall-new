58809a2f9b24546b073b334a824603e1570dcfe2
akarthikkumarGIT <akarthikkumar@gmail.com>
2025-07-30 10:42:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ features {}
 subscription_id = "11da9e6a-2de9-41d7-b691-16e4d50865a9"
 }
 resource "azurerm_resource_group" "app-grp" {
-  name     = "vcs-app-grp"
+  name     = "testvcs-app-grp"
   location = "South Central US"
 }
